f4a49a2de5808b8709f2a8053b07e60f8d7aa1ad
Rubén Blanco <r.blanco.3d@gmail.com>
2024-10-14 23:06:45 +0200

    testIsDivisibleByThree - Coverage 19.05%

--- src/test/java/org/factoriaf5/kata_fizzbuzz_java/FizzbuzzTest.java
+++ src/test/java/org/factoriaf5/kata_fizzbuzz_java/FizzbuzzTest.java
@@ -1,6 +1,27 @@
 package org.factoriaf5.kata_fizzbuzz_java;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class FizzbuzzTest {
     
-    
+    private Fizzbuzz fizzbuzz;
+
+    @BeforeEach
+    public void init() {
+        this.fizzbuzz = new Fizzbuzz();
+    }
+
+    @Test
+    public void testIsDivisibleByThree() {
+
+        int num = 30;
+        
+        boolean result = fizzbuzz.isDivisibleByThree(num);
+
+        assertTrue(result);
+    }
+
 }
