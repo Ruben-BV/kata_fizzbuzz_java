d74d32262f15ae654e6e4c0ef3b4c4deebf0781a
Rubén Blanco <r.blanco.3d@gmail.com>
2024-10-16 02:05:01 +0200

    Agregando Etapa 2 + nuevos tests con Mockito

--- src/main/java/org/factoriaf5/kata_fizzbuzz_java/Fizzbuzz.java
+++ src/main/java/org/factoriaf5/kata_fizzbuzz_java/Fizzbuzz.java
@@ -3,29 +3,36 @@ package org.factoriaf5.kata_fizzbuzz_java;
 public class Fizzbuzz {
 
     public static void main(String[] args) {
-        
-        int number = 32;
+        Fizzbuzz fizzbuzz = new Fizzbuzz();
+        int number = 60;
+        System.out.print(fizzbuzz.fizzBuzz(number));
+    }
+
+
+    public String fizzBuzz(int number) {
         
         Fizzbuzz fizzbuzz = new Fizzbuzz();
         
         boolean result01 = fizzbuzz.isDivisibleByThree(number);
         boolean result02 = fizzbuzz.isDivisibleByFive(number);
+        boolean containsThree = fizzbuzz.containsThree(number);
+        boolean containsFive = fizzbuzz.containsFive(number);
 
 
-        if(result01 == true && result02 == false) {
-            System.out.println("Fizz");
+        if((result01 == true && result02 == false) || containsThree) {
+            return "Fizz";
         }
 
-        else if(result01 == false && result02 == true) {
-            System.out.println("Buzz");
+        else if((result01 == false && result02 == true) || containsFive) {
+            return "Buzz";
         }
 
         else if(result01 == true && result02 == true) {
-            System.out.println("FizzBuzz");
+            return "FizzBuzz";
         }
 
         else {
-            System.out.println(number);
+            return Integer.toString(number);
         }
     }
 
@@ -47,5 +54,20 @@ public class Fizzbuzz {
         else {
             return false;
         }
+    }
+
+    public boolean containsThree(int number) {
+        if(Integer.toString(number).indexOf("3") != -1){
+            return true;
+        }
+        else{return false;}
+        
+    }
+
+    public boolean containsFive(int number) {
+        if(Integer.toString(number).indexOf("5") != -1){
+            return true;
+        }
+        else{return false;}
     }
 }
--- src/test/java/org/factoriaf5/kata_fizzbuzz_java/FizzbuzzTest.java
+++ src/test/java/org/factoriaf5/kata_fizzbuzz_java/FizzbuzzTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mockito;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+
 public class FizzbuzzTest {
     
     private Fizzbuzz fizzbuzz;
@@ -47,10 +52,95 @@ public class FizzbuzzTest {
     @Test
     public void testIsNotDivisibleByFive() {
 
-        int num = 31;
+        int num = 51;
         
         boolean result = fizzbuzz.isDivisibleByFive(num);
 
         assertFalse(result);
+    }
+
+    @Test
+    public void testReturnFizz() {
+
+        int num = 33;
+
+        String result = fizzbuzz.fizzBuzz(num);
+
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void testReturnBuzz() {
+
+        int num = 25;
+
+        String result = fizzbuzz.fizzBuzz(num);
+
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void testReturnFizzBuzz() {
+
+        int num = 60;
+
+        String result = fizzbuzz.fizzBuzz(num);
+
+        assertEquals("FizzBuzz", result);
+    }
+
+    @Test
+    public void testReturnNum() {
+
+        int num = 71;
+
+        String result = fizzbuzz.fizzBuzz(num);
+
+        assertEquals("71", result);
+    }
+
+    @Test
+    public void testContainsThree(){
+        int num = 354;
+        boolean result = fizzbuzz.containsThree(num);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testNotContainsThree(){
+        int num = 50;
+        boolean result = fizzbuzz.containsThree(num);
+        assertFalse(result);
+    }
+
+    @Test
+    public void testContainsFive(){
+        int num = 50;
+        boolean result = fizzbuzz.containsFive(num);
+        assertTrue(result);
+    }
+
+    @Test
+    public void testNotContainsFive(){
+        int num = 122;
+        boolean result = fizzbuzz.containsFive(num);
+        assertFalse(result);
+    }
+    
+    @Test
+    public void testMain() {
+        // Capturar la salida de System.out
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        // Ejecutar el método main
+        Fizzbuzz.main(new String[]{});
+            
+        // Restaurar la salida de System.out
+        System.setOut(originalOut);
+
+        // Verificar la salida
+        assertEquals("FizzBuzz", outContent.toString());
     }
 }
